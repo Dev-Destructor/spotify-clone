1c81488b60e4b732cfc6c9fcce0a2706432c6304
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-06-27 21:19:49 +0530

    added Sidebar and Center components

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,36 @@
-import Head from 'next/head'
-import Sidebar from '../components/sidebar/Sidebar'
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+import Center from "../components/center/Center";
+import Sidebar from "../components/sidebar/Sidebar";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
       <Head>
-        <title>Potyfi</title>
-        <meta name="description" content="All your favourite songs are listed here" />
+        <title>Spotify</title>
+        <meta
+          name="description"
+          content="All your favorite songs are listed here"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=''>
+      <main className="flex">
         <Sidebar />
-        {/* Center */}
+        <Center />
       </main>
 
-      <div>
-        {/* Player */}
-      </div>
+      <div>{/* Player */}</div>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
