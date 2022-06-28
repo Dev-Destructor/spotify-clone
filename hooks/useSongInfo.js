00d0eae88375b9adc33299feddee0ad420267f62
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-06-28 12:07:15 +0530

    hook to get songinfo

---
+++ hooks/useSongInfo.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import useSpotify from "./useSpotify";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState } from "../atoms/songAtom";
+
+function useSongInfo() {
+  const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [songInfo, setSongInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchSongInfo = async () => {
+      const trackInfo = await fetch(
+        `https://api.spotify.com/v1/tracks/${currentTrackId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${spotifyApi.getAccessToken()}`,
+          }
+        }
+      ).then(res => res.json());
+      setSongInfo(trackInfo);
+    }
+    fetchSongInfo();
+  }, [currentTrackId, spotifyApi]);
+
+  return useSongInfo;
+}
+
+export default useSongInfo;
