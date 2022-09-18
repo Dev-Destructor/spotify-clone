421779d50f4625ac28d1ae48992a2cd0be79f6ec
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-09-19 01:25:20 +0530

    fixed env variable issues

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,7 +6,7 @@ export async function middleware(req) {
   const url = req.nextUrl.clone();
   url.pathname = "/login";
 
-  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  const token = await getToken({ req, secret: process.env.NEXT_PUBLIC_JWT_SECRET });
 
   // Allow the request if the its a next-auth session and provider fetching or already contains a token
   if (pathname.includes("/api/auth") || token) {
