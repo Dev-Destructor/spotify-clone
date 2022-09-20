9d2d3cc21ce3d17717cb4416942dd7ffd43e83d8
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-09-20 10:08:34 +0530

    added head tag

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { getProviders, signIn } from "next-auth/react";
+import Head from "next/head";
 
 function login({ providers }) {
   return (
     <div className="flex flex-col items-center justify-center bg-black min-h-screen w-full">
+      <Head>
+        <title>Spotify</title>
+        <meta
+          name="description"
+          content="All your favorite songs are listed here"
+        />
+        <link rel="icon" href="/spotify_icon.ico" />
+      </Head>
       <img
         className="w-52 mb-20"
         src="https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg"
